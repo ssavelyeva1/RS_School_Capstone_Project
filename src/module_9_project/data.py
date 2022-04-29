8613efb41a7a995082ac97014c4e53cf50c852e5
Savelyeva_A <Aliaksandra.Savelyeva@ibm.com>
2022-04-29 15:14:58 +0300

    split dataset function added

--- src/module_9_project/data.py
+++ src/module_9_project/data.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import click
+from sklearn.model_selection import train_test_split
 from module_9_project.config import *
 
 
@@ -9,4 +11,12 @@ def get_dataset():
     return data
 
 
-
+def dataset_split(random_state: int, test_split_ratio: float):
+    data = pd.read_csv(PATH + "train.csv")
+    click.echo(f"Dataset shape: {data.shape}.")
+    features = data.drop("Cover_Type", axis=1)
+    target = data["Cover_Type"]
+    features_train, features_val, target_train, target_val = train_test_split(
+        features, target, test_size=test_split_ratio, random_state=random_state
+    )
+    return features_train, features_val, target_train, target_val
--- src/module_9_project/eda.py
+++ src/module_9_project/eda.py
@@ -4,4 +4,4 @@ from module_9_project.config import *
 
 df = get_dataset()
 profile = ProfileReport(df=df)
-profile.to_file('pandas_profile_test.html')
+profile.to_file("pandas_profile_test.html")

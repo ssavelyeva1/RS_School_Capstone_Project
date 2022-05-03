215812c19e9e968bfb7f1f2245227e35fa788d8b
Savelyeva_A <Aliaksandra.Savelyeva@ibm.com>
2022-05-03 04:02:54 +0300

    noxfile fixed (no tests yet)

--- noxfile.py
+++ noxfile.py
@@ -1,25 +1,27 @@
+import os
 import tempfile
 from typing import Any
 
 import nox
 from nox.sessions import Session
 
-nox.options.sessions = "black", "flake8", "mypy", "tests"
+nox.options.sessions = "black", "flake8", "mypy"  # , "tests"
 locations = "src", "noxfile.py"
 
 
 def install_with_constraints(session: Session, *args: str, **kwargs: Any) -> None:
-    with tempfile.NamedTemporaryFile() as requirements:
-        session.run(
-            "poetry",
-            "export",
-            "--dev",
-            "--format=requirements.txt",
-            "--without-hashes",
-            f"--output={requirements.name}",
-            external=True,
-        )
-        session.install(f"--constraint={requirements.name}", *args, **kwargs)
+    req_path = os.path.join(tempfile.gettempdir(), os.urandom(24).hex())
+    session.run(
+        "poetry",
+        "export",
+        "--dev",
+        "--format=requirements.txt",
+        "--without-hashes",
+        f"--output={req_path}",
+        external=True,
+    )
+    session.install(f"--constraint={req_path}", *args, **kwargs)
+    os.unlink(req_path)
 
 
 @nox.session(python="3.9")
